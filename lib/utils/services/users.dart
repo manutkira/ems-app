f294b5411834e697a0302b3b28ae65d5c7de4405
= <=>
2021-11-02 15:51:47 +0700

    profile edit screen

--- lib/utils/services/users.dart
+++ lib/utils/services/users.dart
@@ -5,34 +5,17 @@ import 'package:ems/utils/services/base_api.dart';
 import 'package:http/http.dart' as http;
 
 class UserService extends BaseService {
-  // Future<UserDummy> getUser() async {
-  //   // var url = "https://laravel-rest-api-app.herokuapp.com/api/users";
-  //   var url = "https://jsonplaceholder.typicode.com/users/1";
-  //   var response = await http.get(Uri.parse(url));
-  //
-  //   return UserDummy.fromJson(json.decode(response.body));
-  // }
-  //
-  // Future<List<UserDummy>> getAllUsers() async {
-  //   // var url = "https://jsonplaceholder.typicode.com/users2";
-  //   var url = "https://laravel-rest-api-app.herokuapp.com/api/users";
-  //   var response = await http.get(Uri.parse(url));
-  //   var data = jsonDecode(response.body);
-  //   return usersFromJson(data);
-  // }
   Future<User> getUser(int id) async {
     var url = "https://laravel-rest-api-app.herokuapp.com/api/users/$id";
-    // var url = "https://jsonplaceholder.typicode.com/users/1";
     var response = await http.get(Uri.parse(url));
-
-    return User.fromJson(json.decode(response.body));
+    var data = json.decode(response.body);
+    return User.fromJson(data);
   }
 
   Future<List<User>> getAllUsers() async {
-    // var url = "https://jsonplaceholder.typicode.com/users2";
     var url = "https://laravel-rest-api-app.herokuapp.com/api/users";
     var response = await http.get(Uri.parse(url));
     var data = jsonDecode(response.body);
-    return usersFromJson(data);
+    return usersFromJson(data['data']);
   }
 }
