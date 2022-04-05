42594e4f90932deb7b376efbd63d0c58657bf7f5
Kim Song <kimsong432@gmail.com>
2022-04-05 10:07:02 +0700

    new icons for admin homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,7 +15,6 @@ import 'package:ems/screens/take_attendance/qr_code_scan.dart';
 import 'package:ems/utils/utils.dart';
 import 'package:ems/widgets/appbar.dart';
 import 'package:ems/widgets/check_status.dart';
-import 'package:ems/widgets/menu_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -202,6 +201,64 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
     await listener.cancel();
   }
 
+  Map<String, List<dynamic>> get menuList {
+    AppLocalizations? local = AppLocalizations.of(context);
+    User user = ref.read(currentUserProvider).user;
+    return {
+      "attendanceAndOvertime": [
+        {
+          "text": local?.attendanceManager,
+          "icon": 'assets/images/attendance.svg',
+          "goto": () => _goToAttendanceScreen(),
+        },
+        {
+          "text": local?.myAttendance,
+          "icon": 'assets/images/my-attendances.svg',
+          "goto": () => _goToMyAttendance(intParse(user.id)),
+        },
+        {
+          "text": local?.overtimeManager,
+          "icon": "assets/images/overtime.svg",
+          "goto": () => _goToOvertimeScreen(),
+        },
+        {
+          "text": local?.myOvertime,
+          "icon": 'assets/images/my-overtime.svg',
+          "goto": () => _goToMyOvertime(intParse(user.id)),
+        },
+      ],
+      "payrollAndLoan": [
+        {
+          "text": local?.payroll,
+          "icon": "assets/images/payroll.svg",
+          "goto": () => _gotoPayroll(),
+        },
+        {
+          "text": local?.myPayroll,
+          "icon": 'assets/images/my-payroll.svg',
+          "goto": () => _goToMyPayroll(intParse(user.id)),
+        },
+        {
+          "text": local?.loan,
+          "icon": "assets/images/loan.svg",
+          "goto": () => _goToLoan(),
+        },
+        {
+          "text": local?.myLoan,
+          "icon": 'assets/images/my-loan.svg',
+          "goto": () => _goToMyLoan(intParse(user.id)),
+        },
+      ],
+      "employees": [
+        {
+          "text": local?.employeeManager,
+          "icon": 'assets/images/employees.svg',
+          "goto": () => _goToEmployeeManager(),
+        }
+      ]
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
@@ -222,7 +279,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             Stack(
               children: [
                 SizedBox(
-                  height: 160,
+                  height: 140,
                   width: _size.width,
                   child: SvgPicture.asset(
                     'assets/images/graph.svg',
@@ -234,7 +291,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
-                    SizedBox(height: 30),
+                    SizedBox(height: 20),
                     CheckStatus(),
                   ],
                 ),
@@ -243,9 +300,9 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
 
             /// check in/out
             Container(
-              height: 170,
+              height: 100,
               padding: const EdgeInsets.symmetric(
-                vertical: 12,
+                // vertical: 12,
                 horizontal: 15,
               ),
               child: ClipRRect(
@@ -288,107 +345,135 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
                 ),
               ),
             ),
-            _buildSpacerVertical,
+            _buildSpacerVertical(),
 
             ValueListenableBuilder(
-              valueListenable:
-                  ref.watch(currentUserProvider).currentUserListenable,
-              builder: (_, Box<User> box, __) {
-                final listFromBox = box.values.toList();
-                final currentUser =
-                    listFromBox.isNotEmpty ? listFromBox[0] : null;
-                return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: GridView.count(
-                      shrinkWrap: true,
-                      physics: const ClampingScrollPhysics(),
-                      // padding: const EdgeInsets.symmetric(horizontal: 16),
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 1,
-                      mainAxisSpacing: 1,
-                      children: [
-                        MenuItem(
-                          onTap: _goToAttendanceScreen,
-                          illustration: SvgPicture.asset(
-                            "assets/images/calendar.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.attendanceManager}",
-                        ),
-                        MenuItem(
-                          onTap: () =>
-                              _goToMyAttendance(currentUser?.id as int),
-                          illustration: SvgPicture.asset(
-                            "assets/images/my-attendances.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.myAttendance}",
-                        ),
-                        MenuItem(
-                          onTap: _goToOvertimeScreen,
-                          illustration: SvgPicture.asset(
-                            "assets/images/overtime-icon.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.overtimeManager}",
-                        ),
-                        MenuItem(
-                          onTap: () => _goToMyOvertime(currentUser),
-                          illustration: SvgPicture.asset(
-                            "assets/images/my-overtime.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.myOvertime}",
-                        ),
-                        MenuItem(
-                          onTap: _gotoPayroll,
-                          illustration: Image.asset(
-                            "assets/images/payroll.png",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.payroll}",
-                        ),
-                        MenuItem(
-                          onTap: () =>
-                              _goToMyPayroll(intParse(currentUser?.id)),
-                          illustration: SvgPicture.asset(
-                            "assets/images/my-payroll.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.myPayroll}",
-                        ),
-                        MenuItem(
-                          onTap: _goToLoan,
-                          illustration: Image.asset(
-                            "assets/images/loan.png",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.loan}",
-                        ),
-                        MenuItem(
-                          onTap: () => _goToMyLoan(intParse(currentUser?.id)),
-                          illustration: SvgPicture.asset(
-                            "assets/images/my-loan.svg",
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.myLoan}",
-                        ),
-                        MenuItem(
-                          onTap: _goToEmployeeManager,
-                          illustration: SvgPicture.asset(
-                            'assets/images/employees-group.svg',
-                            width: MediaQuery.of(context).size.width * 0.17,
-                          ),
-                          label: "${local?.employeeManager}",
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
+                valueListenable:
+                    ref.watch(currentUserProvider).currentUserListenable,
+                builder: (_, Box<User> box, __) {
+                  final listFromBox = box.values.toList();
+                  final currentUser =
+                      listFromBox.isNotEmpty ? listFromBox[0] : null;
+                  return ListView(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    children: [
+                      _buildTitle('${local?.attendanceAndOvertime}'),
+                      _buildSpacerVertical(value: 8.0),
+                      _buildMenuList(menuList['attendanceAndOvertime']!),
+                      _buildSpacerVertical(),
+                      _buildTitle('${local?.payrollAndLoan}'),
+                      _buildSpacerVertical(value: 8.0),
+                      _buildMenuList(menuList['payrollAndLoan']!),
+                      _buildSpacerVertical(),
+                      _buildTitle('${local?.employeeManager}'),
+                      _buildSpacerVertical(value: 8.0),
+                      _buildMenuList(menuList['employees']!)
+                    ],
+                  );
+                }),
+            _buildSpacerVertical(),
+            // grid
+            // ValueListenableBuilder(
+            //   valueListenable:
+            //       ref.watch(currentUserProvider).currentUserListenable,
+            //   builder: (_, Box<User> box, __) {
+            //     final listFromBox = box.values.toList();
+            //     final currentUser =
+            //         listFromBox.isNotEmpty ? listFromBox[0] : null;
+            //     return Container(
+            //       padding: const EdgeInsets.symmetric(horizontal: 16),
+            //       child: ClipRRect(
+            //         borderRadius: BorderRadius.circular(16),
+            //         child: GridView.count(
+            //           shrinkWrap: true,
+            //           physics: const ClampingScrollPhysics(),
+            //           // padding: const EdgeInsets.symmetric(horizontal: 16),
+            //           crossAxisCount: 3,
+            //           crossAxisSpacing: 1,
+            //           mainAxisSpacing: 1,
+            //           children: [
+            //             MenuItem(
+            //               onTap: _goToAttendanceScreen,
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/calendar.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.attendanceManager}",
+            //             ),
+            //             MenuItem(
+            //               onTap: () =>
+            //                   _goToMyAttendance(currentUser?.id as int),
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/my-attendances.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.myAttendance}",
+            //             ),
+            //             MenuItem(
+            //               onTap: _goToOvertimeScreen,
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/overtime-icon.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.overtimeManager}",
+            //             ),
+            //             MenuItem(
+            //               onTap: () => _goToMyOvertime(currentUser),
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/my-overtime.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.myOvertime}",
+            //             ),
+            //             MenuItem(
+            //               onTap: _gotoPayroll,
+            //               illustration: Image.asset(
+            //                 "assets/images/payroll.png",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.payroll}",
+            //             ),
+            //             MenuItem(
+            //               onTap: () =>
+            //                   _goToMyPayroll(intParse(currentUser?.id)),
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/my-payroll.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.myPayroll}",
+            //             ),
+            //             MenuItem(
+            //               onTap: _goToLoan,
+            //               illustration: Image.asset(
+            //                 "assets/images/loan.png",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.loan}",
+            //             ),
+            //             MenuItem(
+            //               onTap: () => _goToMyLoan(intParse(currentUser?.id)),
+            //               illustration: SvgPicture.asset(
+            //                 "assets/images/my-loan.svg",
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.myLoan}",
+            //             ),
+            //             MenuItem(
+            //               onTap: _goToEmployeeManager,
+            //               illustration: SvgPicture.asset(
+            //                 'assets/images/employees-group.svg',
+            //                 width: MediaQuery.of(context).size.width * 0.17,
+            //               ),
+            //               label: "${local?.employeeManager}",
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     );
+            //   },
+            // ),
+            // old old
             //
             // /// current user attendance
             // _buildTitle('${local?.attendance}'),
@@ -435,7 +520,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             //         );
             //       }),
             // ),
-            // _buildSpacerVertical,
+            // _buildSpacerVertical(),
             //
             // /// current user overtime
             // _buildTitle('${local?.overtime}'),
@@ -483,7 +568,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             //     },
             //   ),
             // ),
-            // _buildSpacerVertical,
+            // _buildSpacerVertical(),
             //
             // /// payroll
             // _buildTitle('${local?.payroll}'),
@@ -530,7 +615,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             //         );
             //       }),
             // ),
-            // _buildSpacerVertical,
+            // _buildSpacerVertical(),
             //
             // /// loan
             // _buildTitle('${local?.loan}'),
@@ -576,7 +661,7 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             //         );
             //       }),
             // ),
-            // _buildSpacerVertical,
+            // _buildSpacerVertical(),
             //
             // /// current user overtime
             // _buildTitle('${local?.employee}'),
@@ -621,9 +706,32 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
             //     ),
             //   ),
             // ),
-            // _buildSpacerVertical,
+            // _buildSpacerVertical(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildMenuList(List<dynamic> menuItems) {
+    bool isEnglish = isInEnglish(context);
+    return SizedBox(
+      height: isEnglish ? 135 : 120,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        itemCount: int.parse(
+          '${menuItems.length}',
+        ),
+        separatorBuilder: (_, __) => const SizedBox(width: 16),
+        itemBuilder: (builder, index) {
+          var currentItem = menuItems[index];
+          var text = currentItem['text'];
+          var icon = currentItem['icon'];
+          var goto = currentItem['goto'];
+          return _buildMenuCard(text: text, icon: icon, goto: goto);
+        },
       ),
     );
   }
@@ -643,7 +751,59 @@ class _HomeScreenAdminState extends ConsumerState<HomeScreenAdmin> {
     );
   }
 
-  Widget get _buildSpacerVertical {
-    return const SizedBox(height: 10);
+  Widget _buildSpacerVertical({value = 16.0}) {
+    return SizedBox(height: value);
+  }
+
+  Widget _buildMenuCard(
+      {required String text, required String icon, required Function() goto}) {
+    bool isSvg = icon.substring(icon.length - 3) == 'svg';
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Material(
+        color: kLightBlue,
+        child: InkWell(
+          highlightColor: kBlue.withOpacity(0.25),
+          onTap: goto,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.35,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              // color: kLightBlue,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                isSvg
+                    ? SvgPicture.asset(
+                        icon,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                      )
+                    : Image.asset(
+                        icon,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  child: Text(
+                    text,
+                    style: kSubtitle.copyWith(
+                      fontSize: 12,
+                      color: kBlack,
+                      fontWeight: FontWeight.w700,
+                      height: 1.7,
+                      // overflow: TextOverflow.visible,
+                    ),
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
